146af5a826eb4052e37f20bb3a654686425b8b86
AriadnaPrado <ariadnaprado2002@gmail.com>
2025-10-28 17:05:54 -0300

    Nuevos cambios hay que vincular la navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,10 @@ import { Turnos} from './components/turnos/turnos';
 import { AdminPanel } from './components/admin-panel/admin-panel';
 import {Login} from './components/login/login';
 import {Registro} from './components/registro/registro'
-import { App } from './app';
+
 
 export const routes: Routes = [
- { path: '', pathMatch: 'full', component: App },
+  
   { path: 'turnos', component: Turnos },
   {path: 'admin-panel', component: AdminPanel},
   { path: 'login', component: Login },
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,27 +2,27 @@ import { Component } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: '/app.html',
   styleUrl: '/app.css'
 })
 export class App {
   currentUrl = '';
-
-  constructor(private router: Router) {
+  esInicio = false;
+      constructor(private router: Router) {
     this.router.events
       .pipe(filter(event => event instanceof NavigationEnd))
-      .subscribe((event: any) => {
-        this.currentUrl = event.url;
+      .subscribe((event: NavigationEnd) => {
+        this.esInicio = event.urlAfterRedirects === '/';
       });
   }
-
-  isHomePage(): boolean {
-    return this.currentUrl === '/' || this.currentUrl === '';
   }
-}
+
+
+
+
