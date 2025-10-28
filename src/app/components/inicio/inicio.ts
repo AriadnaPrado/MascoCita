7df4603b299679f92662b42986b7416c7b0431b1
AriadnaPrado <ariadnaprado2002@gmail.com>
2025-10-28 18:22:25 -0300

    Inicio presente

--- src/app/components/inicio/inicio.ts
+++ src/app/components/inicio/inicio.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';        // necesario para ngModel
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
   templateUrl: './inicio.html',
   styleUrl: './inicio.css',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule,RouterLink,RouterLinkActive],
 })
 export class Inicio {
 
