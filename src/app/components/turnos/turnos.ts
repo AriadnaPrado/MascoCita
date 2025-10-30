aeb9890fbb72bf177583c18077a70b9bf66d8cac
AriadnaPrado <ariadnaprado2002@gmail.com>
2025-10-30 12:36:00 -0300

    nuevos agregados

--- src/app/components/turnos/turnos.ts
+++ src/app/components/turnos/turnos.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';        // necesario para ngModel
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 interface Turno {
@@ -13,23 +13,28 @@ interface Turno {
   selector: 'app-turnos',
   templateUrl: './turnos.html',
   styleUrls: ['./turnos.css'],
-  standalone: true,     
+  standalone: true,
   imports: [FormsModule, CommonModule],
 })
 export class Turnos {
   turnos: Turno[] = [
     { fecha: '2025-10-28', hora: '09:00', servicio: 'Corte de cabello', estado: 'Confirmado' },
-    { fecha: '2025-10-29', hora: '11:30', servicio: 'Afeitado', estado: 'Pendiente' }
+    { fecha: '2025-10-29', hora: '11:30', servicio: 'Afeitado', estado: 'Pendiente' },
   ];
 
   nuevoTurno: Turno = { fecha: '', hora: '', servicio: '', estado: 'Pendiente' };
+  mostrarFormulario = false;
+
+  mostrarCampos() {
+    this.mostrarFormulario = !this.mostrarFormulario; // alterna visibilidad
+  }
 
   reservar() {
     if (this.nuevoTurno.fecha && this.nuevoTurno.hora && this.nuevoTurno.servicio) {
       this.turnos.push({ ...this.nuevoTurno });
       this.nuevoTurno = { fecha: '', hora: '', servicio: '', estado: 'Pendiente' };
+      this.mostrarFormulario = false; // oculta el formulario luego de reservar
       alert('Turno reservado con éxito ✅');
     }
   }
 }
-
